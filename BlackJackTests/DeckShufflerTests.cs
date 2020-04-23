4e2be98dc2d1b1f8235c2d638f2f6b50cd8928d0
DevSamaa <samaakanani@gmail.com>
2020-04-23 16:28:13 +1000

    another test

--- BlackJackTests/DeckShufflerTests.cs
+++ BlackJackTests/DeckShufflerTests.cs
@@ -10,18 +10,20 @@ namespace BlackJackTests
         {
         }
 
-        //just a test
+        
         //How can you test whether the ShuffleTheDeck method worked properly?
         //Maybe I can run the method twice and compare that the last item on the list are not equal!?
 
         //var lastCard = deckOfCards[deckOfCards.Count - 1];
 
-        //this is some code11!
+        
 
         [Fact]
         public void Method()
         {
 
         }
+
+        //Test at 16:27
     }
 }
