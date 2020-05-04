4dd38258c5f6d457957dc93296e9e68dd29bfca1
DevSamaa <samaakanani@gmail.com>
2020-05-04 18:47:31 +1000

    first working version with 2 players

--- BlackJackKata/Program.cs
+++ BlackJackKata/Program.cs
@@ -14,34 +14,64 @@ namespace BlackJackKata
             var deck = new Deck();
             var shuffledDeck = deck.CreateShuffledDeck();
 
-            //TODO: once this works (in a refactored way) for 1 player, add player2!
             var gamemaster = new GameMaster();
             var cardHandler = new CardHandler();
 
             var player1 = new Player();
             var player2 = new Player();
-            Player[] allPlayers = { player1, player2 };
 
-            foreach (var player in allPlayers)
+            while (true)
             {
-                int playerScore = 0;
-                while (playerScore < 21)
+                gamemaster.dealInitialCards(cardHandler, shuffledDeck, player1);
+                var player1Score = gamemaster.playGame(player1, cardHandler, shuffledDeck);
+                if (player1Score >21)
                 {
-
-                    gamemaster.dealInitialCards(cardHandler, shuffledDeck, player);
-                    playerScore = gamemaster.playGame(player, cardHandler, shuffledDeck);
-                    Console.WriteLine("You're turn is over");
+                    break;
                 }
-                
+                Console.WriteLine("You're turn is over, the dealers turn begins");
 
+                gamemaster.dealInitialCards(cardHandler, shuffledDeck, player2);
+                var player2Score = gamemaster.playGame(player2, cardHandler, shuffledDeck);
+                if (player2Score > 21)
+                {
+                    break;
+                }
+
+                //check who won
+                if (player1Score > player2Score)
+                {
+                    Console.WriteLine("Player1 wins");
+                }
+                else if (player1Score == player2Score)
+                {
+                    Console.WriteLine("It's a tie! Both player1 and player2 win!");
+                }
+                else
+                {
+                    Console.WriteLine("Player2Wins");
+                }
+
+                break;
             }
-            //TODO check whether the player person has gone bust, if they have the game should be over
-            //TODO I need to somehow return
-            //TODO win at 21! No need to stay! should be automatic
-           
+            
+
+            
 
 
         }
     }
 }
+//TODO win at 21! No need to stay! should be automatic
 
+//Player[] allPlayers = { player1, player2 };
+//foreach (var player in allPlayers)
+//{
+//    int playerScore = 0;
+//    while (playerScore < 21)
+//    {
+
+//        gamemaster.dealInitialCards(cardHandler, shuffledDeck, player);
+//        playerScore = gamemaster.playGame(player, cardHandler, shuffledDeck);
+//        Console.WriteLine("You're turn is over");
+//    }
+//}
